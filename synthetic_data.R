1ab6a99e91677c3a9b12bcc28f4e806bc63ee264
collinschwantes <cjschwantesll@gmail.com>
2019-06-12 21:48:33 -0400

    updating to make example more realistic. Including multiple sites, lat lons

--- synthetic_data.R
+++ synthetic_data.R
@@ -7,13 +7,21 @@ library(tibble)
 library(purrr)
 library(ggplot2)
 
+### need to make this happen at 200 locations
+
 r <- 0.2 
 k <- 30000 
 n <- 100
 t <- 1:50
 
 pops <- list()
-  
+
+FullData <- list()
+
+sites <- paste0("A",1:200) 
+
+for(k in 1:length(sites)){
+ 
 pops[[1]] <- n
   
 for(i in 2:length(t)) {
@@ -25,21 +33,35 @@ for(i in 2:length(t)) {
   print(pops[i])  
 }
   
-
 counts <-  map_dbl(.x = pops,.f = rbind)
+
+lat <- 38.922807 + runif(1,min = -0.00001, max  = 0.00001)
+lon <- -77.068259 + runif(1,min = -0.00001, max  = 0.00001)
 
 SynData <- data.frame(
   dateCollected = Sys.Date() + 1:50,
   TrapType = "Ovisposition",
   Species = "Culex pipiens",
+  Site = sites[k],
   Sex = "Female",
+  lat = lat,
+  lon = lon,
   count =  round(counts,digits = 0)
 )
+
+FullData[[k]] <- SynData
+
+}
 
 
 SynData %>% 
   ggplot(aes(x = Date,y = count)) +
   geom_point()
+
+### add noise in dates
+### add noise in counts
+
+
 
 dir.create("./SyntheticData")
 
